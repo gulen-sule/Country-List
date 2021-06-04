eb0c3b813e10788051da6c0218f94f6e1ed61c0d
gulen-sule <gulensule@gmail.com>
2021-06-04 16:44:55 +0300

    sec commit

--- app/src/main/java/com/example/kotlincountrylist/adapter/MailFragment.kt
+++ app/src/main/java/com/example/kotlincountrylist/adapter/MailFragment.kt
@@ -1,4 +1,34 @@
 package com.example.kotlincountrylist.adapter
 
-class MailFragment {
+import android.content.Intent
+import android.content.IntentSender
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.example.kotlincountrylist.R
+import com.example.kotlincountrylist.databinding.FragmentCountryBinding
+
+class MailFragment : Fragment() {
+    private lateinit var hesap: EditText
+    private lateinit var konu: EditText
+    private lateinit var icerik: EditText
+    private lateinit var send: Button
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        hesap = view.findViewById(R.id.mail_hesabı)
+        konu = view.findViewById(R.id.mail_konusu)
+        icerik = view.findViewById(R.id.mail_icerigi)
+        send = view.findViewById(R.id.sendMail)
+
+        send.setOnClickListener {
+            val intent=Intent(Intent.ACTION_SEND)
+            //intent.putExtra
+
+        }
+        hesap.text.toString()
+    }
+
+
 }
